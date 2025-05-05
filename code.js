332f9769e552a2021a7e5fbacb84ebc23f08936f
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-05-05 14:32:02 -0700

    updates

--- code.js
+++ code.js
@@ -1,1 +1,10 @@
 console.log("You must be one of us!");
+
+const button4 = document.getElementById("exit_add_meal");
+button4.addEventListener("click", Exit_Meal_Window);
+//
+
+//exit allergy window
+function Exit_Allergy_Window() {
+  allergy_window.classList.add("hidden"); // Hide
+}
