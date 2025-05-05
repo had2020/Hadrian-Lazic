2f5d853b1a14462c83d633b87b3baf63764f9295
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-05-05 14:42:34 -0700

    updates

--- code.js
+++ code.js
@@ -1,10 +1,18 @@
 console.log("You must be one of us!");
 
-const button4 = document.getElementById("exit_add_meal");
-button4.addEventListener("click", Exit_Meal_Window);
-//
+/*
+const button1 = document.getElementById("non_sandi");
+button1.addEventListener("click", Non_sandi);
 
-//exit allergy window
-function Exit_Allergy_Window() {
-  allergy_window.classList.add("hidden"); // Hide
+function Non_sandi() {
+  window.location.href =
+    "https://docs.google.com/forms/d/e/1FAIpQLSfd4KXhmels9uDbf7EOE0d3uQzj2ArKcR-aFZceAm5yxDw3Ow/viewform?usp=dialog";
 }
+
+const button2 = document.getElementById("sandi");
+button2.addEventListener("click", Sandi);
+
+function Sandi() {
+  window.location.href = "https://www.example.com";
+}
+*/
